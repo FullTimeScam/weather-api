fce25d35d314cdcf910a174837689c75b3c1cfd1
FullTimeScam <418.gingin@gmail.com>
2024-05-14 10:13:04 +0900

    조건에 따라 날씨 아이콘 적용

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
-import { TiWeatherSunny } from "react-icons/ti";
+import {
+  TiWeatherSunny,
+  TiWeatherDownpour,
+  TiWeatherSnow,
+} from "react-icons/ti";
+import { BsCloudsFill } from "react-icons/bs";
+import { WiDayRain, WiDayStormShowers, WiFog } from "react-icons/wi";
 
 const WeatherIcon = () => {
   const [latitude, setLatitude] = useState();
@@ -35,13 +41,45 @@ const WeatherIcon = () => {
     getWeather();
   }, [latitude]);
 
+  useEffect(() => {
+    if (!weatherData) return;
+    console.log(weatherData.weather[0].icon.substring(0, 2));
+  }, [weatherData]);
+
   if (!weatherData)
     return <div className="w-28 h-12 flex items-center">loading...</div>;
 
   //   return <div>{weatherData && weatherData.name}</div>; //똑같다
   return (
     <div className="text-xs flex items-center gap-2">
-      <TiWeatherSunny size={30} className="text-red-500" />
+      {weatherData.weather[0].icon.substring(0, 2) == "01" && (
+        <TiWeatherSunny size={30} className="text-red-500" />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "02" && (
+        <TiWeatherPartlySunny size={30} />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "03" && (
+        <TiWeatherPartlySunny size={30} />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "04" && (
+        <BsCloudsFill size={30} />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "09" && (
+        <TiWeatherDownpour size={30} className="text-red-500" />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "10" && (
+        <WiDayRain size={30} className="text-red-500" />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "11" && (
+        <WiDayStormShowers size={30} className="text-red-500" />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "13" && (
+        <TiWeatherSnow size={30} className="text-red-500" />
+      )}
+      {weatherData.weather[0].icon.substring(0, 2) == "50" && (
+        <WiFog size={30} className="text-red-500" />
+      )}
+
       {/* <img
         className="w-12 h-12"
         src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} //문서 참고 https://openweathermap.org/weather-conditions
